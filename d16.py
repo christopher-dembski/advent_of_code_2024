aad598868e0c243dec7572f1cd53a7ae48526220
christopher-dembski <christopher_dembski@outlook.com>
2024-12-26 00:30:20 -0500

    Day 16. Part 2.

--- d16.py
+++ d16.py
@@ -51,30 +51,42 @@ def possible_directions(r, c, grid, moving_vertically):
     return directions
 
 
-def part_one(grid, start_r, start_c, end_r, end_c):
+def solve(grid, start_r, start_c, end_r, end_c, part):
     lowest_scores = [[None for _ in row] for row in grid]
     lowest_scores[start_r][start_c] = 0
-    paths = [(start_r, start_c, 0, -1, 0, False)]  # (row, column, delta_r, delta_c, score, rotated)
+    best_paths_positions = set()
+    # [(row, column, delta_r, delta_c, score, rotated, positions), ...]
+    paths = [(start_r, start_c, 0, -1, 0, False, {(start_r, start_c)})]
     while paths:
         new_paths = []
-        new_paths.extend((r, c, *ROTATE_CLOCKWISE[(dr, dc)], score + 1000, True)
-                         for r, c, dr, dc, score, rotated in paths if not rotated)
-        new_paths.extend((r, c, *ROTATE_COUNTER_CLOCKWISE[(dr, dc)], score + 1000, True)
-                         for r, c, dr, dc, score, rotated in paths if not rotated)
-        for r, c, dr, dc, score, rotated in paths:
+        new_paths.extend((r, c, *ROTATE_CLOCKWISE[(dr, dc)], score + 1000, True, ps)
+                         for r, c, dr, dc, score, rotated, ps in paths if not rotated)
+        new_paths.extend((r, c, *ROTATE_COUNTER_CLOCKWISE[(dr, dc)], score + 1000, True, ps)
+                         for r, c, dr, dc, score, rotated, ps in paths if not rotated)
+        for r, c, dr, dc, score, rotated, ps in paths:
             while grid[r][c] != '#':
-                if lowest_scores[r][c] is None or score < lowest_scores[r][c]:
+                ps = {*ps, (r, c)}
+                if lowest_scores[r][c] is None or score <= lowest_scores[r][c]:
+                    if r == end_r and c == end_c:
+                        if lowest_scores[r][c] is not None and score < lowest_scores[r][c]:
+                            best_paths_positions.clear()
+                        best_paths_positions.update(ps)
                     lowest_scores[r][c] = score
                 for new_dr, new_dc in possible_directions(r, c, grid, moving_vertically=bool(dr)):
-                    if lowest_scores[r + new_dr][c + new_dc] is not None:  # do not visit again if seen
-                        continue
-                    new_paths.append((r + new_dr, c + new_dc, new_dr, new_dc, score + 1000 + 1, False))
+                    current_lowest = lowest_scores[r + new_dr][c + new_dc]
+                    candidate_lowest = score + 1000 + 1
+                    if current_lowest is None or candidate_lowest <= current_lowest:
+                        new_paths.append((r + new_dr, c + new_dc, new_dr, new_dc, candidate_lowest, False, ps))
                 r += dr
                 c += dc
                 score += 1
         paths = new_paths
-    return lowest_scores[end_r][end_c]
+    if part == 1:
+        return lowest_scores[end_r][end_c]
+    else:  # part == 2
+        return len(best_paths_positions)
 
 
 if __name__ == '__main__':
-    print(part_one(*parse_input('inputs/d16.txt')))
+    print(solve(*parse_input('inputs/d16.txt'), part=1))
+    print(solve(*parse_input('inputs/d16.txt'), part=2))
