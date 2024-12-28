2f0d6d87efeebdb05a9cc56ac6da29cca07b02f2
christopher-dembski <christopher_dembski@outlook.com>
2024-12-28 15:20:16 -0500

    Day 19. Part 2.

--- d19.py
+++ d19.py
@@ -7,18 +7,32 @@ def parse_input(file_name):
     return towels, designs
 
 
-def can_make_design(towels, design):
-    if design == '':
-        return True
-    for towel in towels:
-        if design.startswith(towel) and can_make_design(towels, design[len(towel):]):
-            return True
-    return False
+def num_designs(towels, initial_design):
+    cache = {}
+
+    def recurse(design):
+        if design in cache:
+            return cache[design]
+        if design == '':
+            return 1
+        total = 0
+        for towel in towels:
+            if design.startswith(towel):
+                total += recurse(design[len(towel):])
+        cache[design] = total
+        return total
+
+    return recurse(initial_design)
 
 
 def part_one(towels, designs):
-    return sum(can_make_design(towels, design) for design in designs)
+    return sum(num_designs(towels, design) > 0 for design in designs)
+
+
+def part_two(towels, designs):
+    return sum(num_designs(towels, design) for design in designs)
 
 
 if __name__ == '__main__':
     print(part_one(*parse_input('inputs/d19.txt')))
+    print(part_two(*parse_input('inputs/d19.txt')))
