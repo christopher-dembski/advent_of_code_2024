41b7ec409fa1538e46855f617b9c84b64783c698
christopher-dembski <christopher_dembski@outlook.com>
2024-12-30 09:25:32 -0500

    Day 23. Part 2.

--- d23.py
+++ d23.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import itertools as it
 
 
 def parse_input(file_name):
@@ -26,5 +27,22 @@ def part_one(connections):
     return sum(c1.startswith('t') or c2.startswith('t') or c3.startswith('t') for c1, c2, c3 in groups)
 
 
+def get_largest_group(graph):
+    largest_group = set()
+    for c, connections in graph.items():
+        for size in range(len(connections) + 1, len(largest_group), -1):
+            combinations = it.combinations(connections, r=(size - 1))
+            for combination in combinations:
+                nodes = {c, *combination}
+                if all((nodes - {node}).issubset(graph[node]) for node in nodes):
+                    largest_group = nodes
+    return largest_group
+
+
+def part_two(connections):
+    return ','.join(sorted(get_largest_group(build_graph(connections))))
+
+
 if __name__ == '__main__':
     print(part_one(parse_input('inputs/d23.txt')))
+    print(part_two(parse_input('inputs/d23.txt')))
