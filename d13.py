96d7e3d5d3391f2c23d3289e48c1ca3497474676
christopher-dembski <christopher_dembski@outlook.com>
2025-08-09 13:11:19 -0400

    Day 13. Part 2.

--- d13.py
+++ d13.py
@@ -1,5 +1,7 @@
 import re
+from decimal import Decimal, getcontext
 
+getcontext().prec = 50
 
 def parse_position(string):
     x, y = re.findall('\d+', string)
@@ -43,5 +45,32 @@ def part_one(games):
     return sum(min_tokens for min_tokens in token_counts if min_tokens)
 
 
+def is_integer(n):
+    return abs(n - round(n)) < Decimal(0.0001)
+
+
+def solve(ax, ay, bx, by, x, y):
+    ax, ay, bx, by, x, y = Decimal(ax), Decimal(ay), Decimal(bx), Decimal(by), Decimal(x), Decimal(y)
+    x += 10000000000000
+    y += 10000000000000
+    b = (y - ay * x / ax) / (by - ay * bx / ax)
+    a = (x - bx * b) / ax
+    return a, b
+
+
+# works for part 1 inputs, but not part 2 with added 10000000000000
+# possibly floating point precision issue
+def part_two(games):
+    total = 0
+    for game in games:
+        a, b = solve(*game[0], *game[1], *game[2])
+        if not (is_integer(a) and is_integer(b)):
+            continue
+        total += 3 * round(a)
+        total += round(b)
+    return total
+
+
 if __name__ == '__main__':
-    print(part_one(parse_input('inputs/d13.txt')))
+    # print(part_one(parse_input('inputs/d13.txt')))
+    print(part_two(parse_input('inputs/d13.txt')))
